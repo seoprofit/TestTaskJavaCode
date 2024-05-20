ed1efa843fee5d9b68734aca7800f647a6a1f84b
Kirill Vorobyev <seoprofit@mail.ru>
2024-05-20 22:02:33 +0400

    Apply transactional to repository

--- src/main/java/com/example/testtaskjavacode/repository/WalletRepository.java
+++ src/main/java/com/example/testtaskjavacode/repository/WalletRepository.java
@@ -1,11 +1,25 @@
 package com.example.testtaskjavacode.repository;
 
 import com.example.testtaskjavacode.DAO.Wallet;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+@Repository
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+
     Optional<Wallet> findByuuid(UUID valetId);
+    List<Wallet> findAll();
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Wallet save(Wallet newWallet);
+
+
+
+
+
 }
